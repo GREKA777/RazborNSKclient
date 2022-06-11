d2737615d62026a8a5ccf5a0cc7dd4a643e896e8
GREKA <egorka.gre4ko@gmail.com>
2022-06-11 19:21:51 +0700

    razborka1

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "http://62.113.111.238:5000/"
 })
 
 const $authHost = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "http://62.113.111.238:5000/"
 })
 
 const authInterceptor = (config) => {
